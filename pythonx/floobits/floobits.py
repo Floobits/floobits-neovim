12e1868434b48a38ca49831bf28970322fc471a4
Bjorn Tipling <bjorn@ambientchill.com>
2014-11-17 18:55:19 -0800

    Update plugin version. #6

--- pythonx/floobits/floobits.py
+++ pythonx/floobits/floobits.py
@@ -39,7 +39,7 @@ reactor = reactor.reactor
 
 # Protocol version
 G.__VERSION__ = '0.11'
-G.__PLUGIN_VERSION__ = '3.0.5'
+G.__PLUGIN_VERSION__ = '4.0.0'
 
 G.LOG_TO_CONSOLE = False
 G.CHAT_VIEW = True
