dbfa051e4f097dfa3f46997a2019556a62861258
Geoff Greer <geoff@greer.fm>
2021-10-17 22:48:14 -0700

    Bump version.

--- rplugin/python/floobits/__init__.py
+++ rplugin/python/floobits/__init__.py
@@ -45,7 +45,7 @@ reactor = reactor.reactor
 
 # Protocol version
 G.__VERSION__ = '0.11'
-G.__PLUGIN_VERSION__ = '3.4.3'
+G.__PLUGIN_VERSION__ = '3.4.4'
 
 G.LOG_TO_CONSOLE = False
 G.CHAT_VIEW = True
